bf9b155440fc6079c105b77e6dc7f1127a8f25f3
Cristian Zambrano <criszamcham@gmail.com>
2026-02-04 13:26:13 -0700

    Update data assets import path

--- js/data/carouselData.js
+++ js/data/carouselData.js
@@ -1,43 +1,41 @@
-const BASE_PATH = window.location.hostname.includes("github.io") ? "/triumph" : "";
-
 export const volunteerCards = [
   {
-    image: `${BASE_PATH}/assets/carousel/jewish.jpg`,
+    image: './assets/carousel/jewish.jpg',
     title: "Jews",
     alt: "Jewish person in a church"
   },
   {
-    image: `${BASE_PATH}/assets/carousel/prayerAndCare.jpg`,
+    image: './assets/carousel/prayerAndCare.jpg',
     title: "Prayer & Care",
     alt: "Man and woman holding a lit candle"
   },
   {
-    image: `${BASE_PATH}/assets/carousel/kids.jpg`,
+    image: './assets/carousel/kids.jpg',
     title: "Kids",
     alt: "Young girl smiling"
   },
   {
-    image: `${BASE_PATH}/assets/carousel/students.jpg`,
+    image: './assets/carousel/students.jpg',
     title: "Students",
     alt: "Young girl smiling"
   },
   {
-    image: `${BASE_PATH}/assets/carousel/worship.jpg`,
+    image: './assets/carousel/worship.jpg',
     title: "Worship",
     alt: "Woman singing into a microphone"
   },
   {
-    image: `${BASE_PATH}/assets/carousel/men.jpg`,
+    image: './assets/carousel/men.jpg',
     title: "Men",
     alt: "Group of men smiling"
   },
   {
-    image: `${BASE_PATH}/assets/carousel/women.jpg`,
+    image: './assets/carousel/women.jpg',
     title: "Women",
     alt: "Group of women smiling"
   },
   {
-    image: `${BASE_PATH}/assets/carousel/smallGroups.jpg`,
+    image: './assets/carousel/smallGroups.jpg',
     title: "Small Groups",
     alt: "People in a small group discussion in a house"
   }
--- js/data/missionIconCardsData.js
+++ js/data/missionIconCardsData.js
@@ -1,22 +1,20 @@
-const BASE_PATH = window.location.hostname.includes("github.io") ? "/triumph" : "";
-
 export const iconCardsList = [
   {
-    icon: `${BASE_PATH}/assets/icons/play.svg`,
+    icon: './assets/icons/play.svg',
     title: 'Watch a Message',
     description: 'Catch up on the latest and past weekend service messages anytime, anywhere.',
     linkText: 'Catch Our Latest Message',
     linkUrl: '#'
   },
   {
-    icon: `${BASE_PATH}/assets/icons/location.svg`,
+    icon: './assets/icons/location.svg',
     title: 'Find a Location',
     description: 'Easily find a campus near you. Join us in person and get connected.',
     linkText: 'Search Our Campuses',
     linkUrl: '#'
   },
   {
-    icon: `${BASE_PATH}/assets/icons/message.svg`,
+    icon: './assets/icons/message.svg',
     title: 'Get Connected',
     description: 'Stay connected and in the loop—sign up for updates, events, and ways to get involved.',
     linkText: 'Contact Us',
