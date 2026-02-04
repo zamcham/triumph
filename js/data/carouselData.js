2743bae699f24b987e454709021dc62efb334832
Cristian Zambrano <criszamcham@gmail.com>
2026-02-04 13:04:57 -0700

    Update paths for deployment

--- js/data/carouselData.js
+++ js/data/carouselData.js
@@ -1,41 +1,41 @@
 export const volunteerCards = [
   {
-    image: "/assets/carousel/jewish.jpg",
+    image: "../../assets/carousel/jewish.jpg",
     title: "Jews",
     alt: "Jewish person in a church"
   },
   {
-    image: "/assets/carousel/prayerAndCare.jpg",
+    image: "../../assets/carousel/prayerAndCare.jpg",
     title: "Prayer & Care",
     alt: "Man and woman holding a lit candle"
   },
   {
-    image: "/assets/carousel/kids.jpg",
+    image: "../../assets/carousel/kids.jpg",
     title: "Kids",
     alt: "Young girl smiling"
   },
   {
-    image: "/assets/carousel/students.jpg",
+    image: "../../assets/carousel/students.jpg",
     title: "Students",
     alt: "Young girl smiling"
   },
   {
-    image: "/assets/carousel/worship.jpg",
+    image: "../../assets/carousel/worship.jpg",
     title: "Worship",
     alt: "Woman singing into a microphone"
   },
   {
-    image: "/assets/carousel/men.jpg",
+    image: "../../assets/carousel/men.jpg",
     title: "Men",
     alt: "Group of  men smiling"
   },
   {
-    image: "/assets/carousel/women.jpg",
+    image: "../../assets/carousel/women.jpg",
     title: "Women",
     alt: "Group of women smiling"
   },
   {
-    image: "/assets/carousel/smallGroups.jpg",
+    image: "../../assets/carousel/smallGroups.jpg",
     title: "Small Groups",
     alt: "People in a small group discussion in a house"
   }
