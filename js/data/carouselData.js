c62a75ccfb70e10945bb9ec71458b00afe429a4e
Cristian Zambrano <criszamcham@gmail.com>
2026-02-04 13:22:42 -0700

    Fix path issue on data constant

--- js/data/carouselData.js
+++ js/data/carouselData.js
@@ -1,41 +1,43 @@
+const BASE_PATH = window.location.hostname.includes("github.io") ? "/triumph" : "";
+
 export const volunteerCards = [
   {
-    image: "../../assets/carousel/jewish.jpg",
+    image: `${BASE_PATH}/assets/carousel/jewish.jpg`,
     title: "Jews",
     alt: "Jewish person in a church"
   },
   {
-    image: "../../assets/carousel/prayerAndCare.jpg",
+    image: `${BASE_PATH}/assets/carousel/prayerAndCare.jpg`,
     title: "Prayer & Care",
     alt: "Man and woman holding a lit candle"
   },
   {
-    image: "../../assets/carousel/kids.jpg",
+    image: `${BASE_PATH}/assets/carousel/kids.jpg`,
     title: "Kids",
     alt: "Young girl smiling"
   },
   {
-    image: "../../assets/carousel/students.jpg",
+    image: `${BASE_PATH}/assets/carousel/students.jpg`,
     title: "Students",
     alt: "Young girl smiling"
   },
   {
-    image: "../../assets/carousel/worship.jpg",
+    image: `${BASE_PATH}/assets/carousel/worship.jpg`,
     title: "Worship",
     alt: "Woman singing into a microphone"
   },
   {
-    image: "../../assets/carousel/men.jpg",
+    image: `${BASE_PATH}/assets/carousel/men.jpg`,
     title: "Men",
-    alt: "Group of  men smiling"
+    alt: "Group of men smiling"
   },
   {
-    image: "../../assets/carousel/women.jpg",
+    image: `${BASE_PATH}/assets/carousel/women.jpg`,
     title: "Women",
     alt: "Group of women smiling"
   },
   {
-    image: "../../assets/carousel/smallGroups.jpg",
+    image: `${BASE_PATH}/assets/carousel/smallGroups.jpg`,
     title: "Small Groups",
     alt: "People in a small group discussion in a house"
   }
